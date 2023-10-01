f70b3559ca2a957a3dcaf0a4a48c3462a3bc0d44
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2023-10-01 23:38:06 +0100

    structs

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zip   int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Anderson",
+		contactInfo: contactInfo{
+			email: "jim@mail.com",
+			zip:   94000,
+		},
+	}
+
+	jim.updateName("Jimmy")
+	jim.print()
+
+}
+
+func (p *person) updateName(newFirstName string) {
+	(*p).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
